23ee4bb4a049776a5134052c934e7a04fdacb752
Pepesaur96 <jjvazquez96@gmail.com>
2023-09-22 20:44:45 +0000

    bettyfix

--- get_op_code.c
+++ get_op_code.c
@@ -9,31 +9,31 @@
  */
 void (*get_op_code(char *token, unsigned int line))(stack_t **, unsigned int)
 {
-    int i;
-    instruction_t operation[] = {
-        {"push", push_stack},
-        {"pall", pall_stack},
-        {"pint", pint_stack},
-        {"pop", pop_stack},
-        {"swap", _swap},
-        {"nop", _nop},
-        {"add", _add},
-        {"sub", _sub},
-        {"div", _div},
-        {"mul", _mul},
-        {"mod", _mod},
-        {"rotl", rotl_stack},
-        {"rotr", rotr_stack},
-        {"pchar", _pchar},
-        {NULL, NULL}};
-    for (i = 0; operation[i].opcode != NULL; i++)
-    {
-        if (strcmp(token, operation[i].opcode) == 0)
-        {
-            return (operation[i].f);
-        }
-    }
-    /* if not found*/
-    fprintf(stderr, "L%u: unknown instruction %s\n", line, token);
-    exit(EXIT_FAILURE);
+	int i;
+	instruction_t operation[] = {
+		{"push", push_stack},
+		{"pall", pall_stack},
+		{"pint", pint_stack},
+		{"pop", pop_stack},
+		{"swap", _swap},
+		{"nop", _nop},
+		{"add", _add},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"rotl", rotl_stack},
+		{"rotr", rotr_stack},
+		{"pchar", _pchar},
+		{NULL, NULL}};
+	for (i = 0; operation[i].opcode != NULL; i++)
+	{
+		if (strcmp(token, operation[i].opcode) == 0)
+		{
+			return (operation[i].f);
+		}
+	}
+	/* if not found*/
+	fprintf(stderr, "L%u: unknown instruction %s\n", line, token);
+	exit(EXIT_FAILURE);
 }
