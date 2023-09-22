d1624fb559e4a914a108aa41763c959c470808ee
Pepesaur96 <jjvazquez96@gmail.com>
2023-09-22 20:56:20 +0000

    betty fix

--- main.c
+++ main.c
@@ -8,12 +8,12 @@
  */
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);  
-    }
-        
-    monty_prog(argv);
-    return (0);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	monty_prog(argv);
+	return (0);
 }
